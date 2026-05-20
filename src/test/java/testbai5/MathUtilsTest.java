d61b52b292aeba26f252ce6e84c83857bc31a627
HuuTam <25020359@vnu.edu.vn>
2026-05-20 11:07:06 +0700

    test: kích hoạt lại bot kiểm tra định dạng

--- src/test/java/testbai5/MathUtilsTest.java
+++ src/test/java/testbai5/MathUtilsTest.java
@@ -8,17 +8,15 @@ public class MathUtilsTest {
     int So_Nguyen_Sai_Dinh_Dang = 10;
     @Test
     public void testMax() {
+
+
+
+
+
+
         assertEquals(          3, MathUtils.max(3, 5)    ); //Sửa dẻ cố tình trả về kết quả sai
         assertEquals(7, MathUtils.max(7, 2));
         assertEquals(2, MathUtils.max(2, 2));
-
-
-
-
-
-
-
-
     }
 }
 
