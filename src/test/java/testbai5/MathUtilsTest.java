b69ca037bcc7ce290c2e60b2037945453c69500a
HuuTam <25020359@vnu.edu.vn>
2026-05-20 10:56:07 +0700

    test: co tinh tao them loi checkstyle goi bot

--- src/test/java/testbai5/MathUtilsTest.java
+++ src/test/java/testbai5/MathUtilsTest.java
@@ -11,6 +11,12 @@ public class MathUtilsTest {
         assertEquals(          3, MathUtils.max(3, 5)    ); //Sửa dẻ cố tình trả về kết quả sai
         assertEquals(7, MathUtils.max(7, 2));
         assertEquals(2, MathUtils.max(2, 2));
+
+
+
+
+
+
     }
 }
 
