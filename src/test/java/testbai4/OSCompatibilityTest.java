b90f0a11ba53fca2e600bafa61dc8102024046c1
HuuTam <25020359@vnu.edu.vn>
2026-05-20 08:57:58 +0700

    feat: bổ sung cấu hình plugin jacoco hoàn chỉnh bài 5

--- src/test/java/testbai4/OSCompatibilityTest.java
+++ src/test/java/testbai4/OSCompatibilityTest.java
@@ -9,17 +9,13 @@ public class OSCompatibilityTest {
 
     @Test
     public void testWindowsPathStyle() throws IOException {
-        // Cố tình viết cứng đường dẫn dùng dấu gạch chéo ngược của Windows
         String smartPath = "target" + File.separator + "test-folder" +  File.separator + "temp.txt";
 
         File file = new File(smartPath);
-        // Tạo thư mục cha nếu chưa có
         file.getParentFile().mkdirs();
 
-        // Tạo file mới
-        boolean isCreated = file.createNewFile();
+        boolean newFile = file.createNewFile();
 
-        // Kiểm tra xem file có tồn tại thực sự không
         assertTrue(file.exists());
 
         file.delete();
